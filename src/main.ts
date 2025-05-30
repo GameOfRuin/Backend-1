1dbe13f0317844db7151d2fd600429f443f5c961
Anatoly Kochetov <tolik.kochetov44@gmail.com>
2025-05-30 16:23:54 +0300

    feat: Запуск сервера, получение запроса

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,13 @@
-console.log(2);
-fs;
+import express from 'express';
+
+const server = express();
+
+server.get('/', (req, res) => {
+  console.log('Пришел Get');
+
+  res.json({ figure: '2' });
+});
+
+server.listen(3000, () => {
+  console.log('Get');
+});
