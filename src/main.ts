08c8b03ef9850ea36e156d63f43c3adfe03e7e0c
Anatoly Kochetov <tolik.kochetov44@gmail.com>
2025-05-31 17:46:29 +0300

    feat: Добавил работу с query, path, body

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,33 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 const server = express();
 
-server.get('/', (req, res) => {
-  console.log('Пришел Get');
+server.use(express.json());
 
-  res.json({ figure: '2' });
+server.get('/', (req: Request, res: Response) => {
+  console.log('Пришел Get');
+});
+
+server.get('/script/complexScripts', (req: Request, res: Response) => {
+  const filterComplexScripts = req.query;
+  res.json(filterComplexScripts);
+  console.log(`Пришел запрос на сложные скрипты`);
+});
+
+server.get('/script/:numberScript', (req: Request, res: Response) => {
+  const numberScript = req.params.numberScript;
+  console.log(`Пришел запрос на номер скрипта ${numberScript}`);
+});
+
+server.get('/script/complexScripts/:number', (req: Request, res: Response) => {
+  const numberScript = req.params.number;
+  console.log(`Пришел запрос на номер сложного скрипта ${numberScript}`);
+});
+
+server.post('/register', (req: Request, res: Response) => {
+  const numberScript = req.body;
+  console.log(`Пришли данные для регистрации ${numberScript}`);
+  res.json(numberScript);
 });
 
 server.listen(3000, () => {
