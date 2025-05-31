e6c449cae9bebdcb8a085fcd180cc8dd66ff9419
Anatoly Kochetov <tolik.kochetov44@gmail.com>
2025-05-31 18:11:59 +0300

    feat: Добавил логгер

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,45 @@
 import express, { Request, Response } from 'express';
+import logger from './logger';
 
 const server = express();
 
 server.use(express.json());
 
 server.get('/', (req: Request, res: Response) => {
-  console.log('Пришел Get');
+  logger.info('Пришел Get');
 });
 
 server.get('/script/complexScripts', (req: Request, res: Response) => {
   const filterComplexScripts = req.query;
   res.json(filterComplexScripts);
-  console.log(`Пришел запрос на сложные скрипты`);
+  logger.info(`Пришел запрос на сложные скрипты`);
 });
 
 server.get('/script/:numberScript', (req: Request, res: Response) => {
   const numberScript = req.params.numberScript;
-  console.log(`Пришел запрос на номер скрипта ${numberScript}`);
+  if (Number(numberScript) < 100) {
+    res.json({ error: 'Ваш скрипт' });
+    logger.info('Запрос  скрипта');
+  } else {
+    res.json({ error: 'Такого скрипта не существует' });
+    logger.error('Запрос несуществующего скрипта');
+  }
 });
 
 server.get('/script/complexScripts/:number', (req: Request, res: Response) => {
-  const numberScript = req.params.number;
-  console.log(`Пришел запрос на номер сложного скрипта ${numberScript}`);
+  const numberComplexScript = req.params.number;
+  if (Number(numberComplexScript) < 5) {
+    res.json({ error: 'Ваш скрипт' });
+    logger.info(`Пришел запрос на номер сложного скрипта ${numberComplexScript}`);
+  } else {
+    res.json({ error: 'Такого скрипта не существует' });
+    logger.error('Запрос несуществующего скрипта');
+  }
 });
 
 server.post('/register', (req: Request, res: Response) => {
   const numberScript = req.body;
-  console.log(`Пришли данные для регистрации ${numberScript}`);
+  logger.info(`Пришли данные для регистрации. name = ${numberScript.name} и password = ${numberScript.password}`);
   res.json(numberScript);
 });
 
